645cc16e313853ae301a52c506e25b433a32c799
RottenLog <ivanlogna@gmail.com>
2024-02-13 21:12:43 +0300

    backup before LayoutBuilder

--- lib/feauteres/main_screen/main_screen.dart
+++ lib/feauteres/main_screen/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});  
@@ -9,7 +10,17 @@ class MainScreen extends StatelessWidget {
         actions: <Widget>[
           Expanded(
             child: IconButton(
+              icon: const FaIcon(FontAwesomeIcons.user, size: 40,),
+              hoverColor: Colors.blueAccent.shade100,
+              onPressed: () {
+                Navigator.pushNamed(context, '/gift');
+              },
+            ),
+          ),
+          Expanded(
+            child: IconButton(
               icon: const Icon(Icons.card_giftcard, size: 40,),
+              hoverColor: Colors.blueAccent.shade100,
               onPressed: () {
                 Navigator.pushNamed(context, '/gift');
               },
@@ -17,35 +28,15 @@ class MainScreen extends StatelessWidget {
           ),
           Expanded(
             child: IconButton(
-              icon: const Icon(Icons.money, size: 40,),
+              icon: const Icon(Icons.settings, size: 40,),
+              hoverColor: Colors.blueAccent.shade100,
               onPressed: () {
                 Navigator.pushNamed(context, '/gift');
               },
             ),
           ),
-          Expanded(
-            child: IconButton(
-              icon: const Icon(Icons.gif_box_outlined, size: 40,),
-              onPressed: () {
-                Navigator.pushNamed(context, '/gift');
-              },
-            ),
-          ),
-          Expanded(
-            child: IconButton(
-              icon: const Icon(Icons.gif_box_outlined, size: 40,),
-              onPressed: () {
-                Navigator.pushNamed(context, '/gift');
-              },
-            ),
-          ),
-          Expanded(
-            child: IconButton(
-              icon: const Icon(Icons.gif_box_outlined, size: 40,),
-              onPressed: () {
-                Navigator.pushNamed(context, '/gift');
-              },
-            ),
+          const Expanded(
+            child: Text('123'),
           ),
         ]
       ),
